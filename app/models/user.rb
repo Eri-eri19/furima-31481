6c521ad3d5f23803dd9e4d2e9f53e3ab154f83ec
りえ ちゃん <echann41@gmail.com>
2021-02-16 12:29:07 +0900

    ログイン・ログアウト機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+   with_options presence: true do
+    validates :nickname
+    NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+    NAME_MESSAGE1 = '全角ひらがな、全角カタカナ、漢字のみで入力して下さい'.freeze
+    validates :lastname, format: { with: NAME_REGEX, message: NAME_MESSAGE1 }
+    validates :firstname, format: { with: NAME_REGEX, message: NAME_MESSAGE1 }
+    NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+    NAME_MESSAGE2 = '全角カタカナのみで入力して下さい'.freeze
+    validates :lastnamekana, format: { with: NAME_KANA_REGEX, message: NAME_MESSAGE2 }
+    validates :firstnamekana, format: { with: NAME_KANA_REGEX, message: NAME_MESSAGE2 }
+    validates :birthday
+  end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX }, length: { minimum: 6 }
+
 end
+
+
